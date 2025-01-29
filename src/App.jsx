649f6a64220ef264b5fdeeeeb1909aec47330d00
Manisha <110734103+manisha574@users.noreply.github.com>
2025-01-29 17:43:23 +0530

    Fix routing issue for subdirectory deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
- import About from './pages/About';
- import CV from './pages/CV';
- import Contact from './pages/Contact';
- import Experience from './pages/Experience';
+import About from './pages/About';
+import CV from './pages/CV';
+import Contact from './pages/Contact';
+import Experience from './pages/Experience';
 
 function App() {
   return (
-    <Router>
+    // Set the basename to '/portfolio' (or the name of your repo/subdirectory)
+    <Router basename="/portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-          <Route path="/cv" element={<CV />} /> 
-        <Route path="/contact" element={<Contact />} />  
-        <Route path="/experience" element={<Experience/>}/>
+        <Route path="/cv" element={<CV />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
     </Router>
   );
-};
-export default App; 
+}
 
+export default App;
