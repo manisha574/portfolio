f22eddf08c670a3723447c253748192b0575aaee
Manisha <110734103+manisha574@users.noreply.github.com>
2025-01-29 18:46:07 +0530

    Fix routing issue with _redirects file for Netlify

--- src/Component/TempHeader.jsx
+++ src/Component/TempHeader.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className="p-4 bg-gray-100 shadow-md">
@@ -5,44 +7,44 @@ const Header = () => {
       <nav className="mt-4">
         <ul className="flex gap-4 text-lg ">
           <li>
-            <a
-              href="/"
+            <Link
+              to="/"
               className="hover:text-indigo-600 transition-colors duration-300"
             >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/cv"
+            <Link
+              to="/cv"
               className="hover:text-indigo-600 transition-colors duration-300"
             > 
               CV
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/contact"
+            <Link
+              to="/contact"
               className="hover:text-indigo-600 transition-colors duration-300"
             >
               Contact
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/about"
+            <Link
+              to="/about"
               className="hover:text-indigo-600 transition-colors duration-300"
             >
               About Me
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/experience"
+            <Link
+              to="/experience"
               className="hover:text-indigo-600 transition-colors duration-300"
             >
               Experience
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
